20d81acd4e5b73cc894f0a69c77496833214a34e
DjairCosta <riajd@gmail.com>
2022-03-02 17:07:19 -0300

    First commit

---
+++ src/test/java/com/daytech/javaangular/JavaangularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daytech.javaangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaangularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
